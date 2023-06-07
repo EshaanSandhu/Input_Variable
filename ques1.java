f8cffd1beeeeab857e61070a0147ea5250f48b36
EshaanSandhu <110472900+EshaanSandhu@users.noreply.github.com>
2023-06-07 07:18:00 +0530

    Add files via upload

---
+++ ques1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ques1{
+public static void main(String args[]){
+Scanner scanner = new Scanner(System.in);
+System.out.println("Enter int value:");
+int a=scanner.nextInt();
+System.out.println("A="+a);
+System.out.println("Enter float value:");
+float b=scanner.nextFloat();
+System.out.println("B="+b);
+System.out.println("Enter double value:");
+double c=scanner.nextDouble();
+System.out.println("C="+c);
+System.out.println("Enter char value:");
+String d=scanner.nextLine();
+System.out.println("D="+d);
+}
+}
